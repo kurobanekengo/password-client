40f8ea9a8b20b80009dee9dbc782a5ad1756cbc0
kurobanekengo <kurobanekengo@gmail.com>
2020-03-20 13:46:22 +0900

    css導入準備

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,39 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const sourcePath = path.resolve(__dirname, './source');
-const outputPath = path.resolve(__dirname, './public/js');
+const outputPath = path.resolve(__dirname, './public');
 const htmlContentPath = path.resolve(__dirname, './public');
 
 module.exports = {
     mode: 'development',
     entry: {
-        app: [
+        "js/index": [
             'webpack-dev-server/client?http://192.168.33.10:8080',
             'webpack/hot/dev-server',
             `${sourcePath}/index.tsx`
-        ]
+        ],
+        "style/style": `${sourcePath}/style/style.scss`
     },
     output: {
-        filename: 'index.js',
         path:  outputPath
     },
     module: {
         rules:[
             {
-                use: 'ts-loader',
                 test: /\.tsx?$/,
+                use: 'ts-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
             }
         ]
     },
@@ -42,6 +52,7 @@ module.exports = {
         inline: true
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
     ]
 };
